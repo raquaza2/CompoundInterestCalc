89f11ab33d73e7953da07a239bfbe9f346419268
raquaza2 <57903323+raquaza2@users.noreply.github.com>
2024-10-05 23:49:36 +0800

    with "<="

--- CompIntCalc.py
+++ CompIntCalc.py
@@ -6,5 +6,16 @@ while principle <= 0:
     principle = float(input("Enter the principal amount: "))
     if principle <= 0:
         print("Please enter a positive number")
-        
-print(principle)
+
+while rate <= 0:
+    rate = float(input("Enter the rate amount: "))
+    if rate <= 0:
+        print("Please enter a positive number")    
+
+while time <= 0:
+    time = int(input("Enter the number of years: "))
+    if time <= 0:
+        print("Please enter a positive number")
+
+total = principle * pow((1 + rate/100), time)
+print(f"The total amount after{time} years is: {total:.2f}")
