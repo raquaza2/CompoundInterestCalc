38c243c70144197306594b7302c95145f1bb84c8
raquaza2 <57903323+raquaza2@users.noreply.github.com>
2024-10-05 23:54:34 +0800

    using "while True"

--- CompIntCalc.py
+++ CompIntCalc.py
@@ -2,20 +2,26 @@ principle = 0
 rate = 0
 time = 0
 
-while principle <= 0:
+while True:
     principle = float(input("Enter the principal amount: "))
     if principle <= 0:
         print("Please enter a positive number")
+    else:
+        break
 
-while rate <= 0:
+while True:
     rate = float(input("Enter the rate amount: "))
     if rate <= 0:
         print("Please enter a positive number")    
+    else:
+        break
 
-while time <= 0:
+while True:
     time = int(input("Enter the number of years: "))
     if time <= 0:
         print("Please enter a positive number")
-
+    else:
+        break
+    
 total = principle * pow((1 + rate/100), time)
 print(f"The total amount after{time} years is: {total:.2f}")
